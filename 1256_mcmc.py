4f6f56e0fd0298cd558f28e2e12d8aea7c68c08f
ECGonzales <gonza338@gmail.com>
2017-04-10 15:29:54 -0400

    Updated filepath and pep_8 fixes

--- 1256_mcmc.py
+++ 1256_mcmc.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # Read in SED of 1256-0224
-SED_path = '/Users/EileenGonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/Atmospheres_paper/Redone/best1256-0224 (L3.5sd) SED.txt'
+SED_path = '../Atmospheres_paper/Redone/best1256-0224 (L3.5sd) SED.txt'  # Move up to Modules and into folder
 w, f, e = np.loadtxt(SED_path, delimiter=" ", unpack=True)
 
 # Adding units to the arrays
@@ -26,27 +26,6 @@ mg = mc.make_model_db('btsettl', 'model_atmosphere_db', model_grid=models, grid_
                       param_lims=[('teff', 1400, 2000, 50), ('logg', 3.5, 5.5, 0.5)], fill_holes=False, bands=[],
                       rebin_models=w, use_pandas=False)
 
-# I can't get the model grid to be made error:
-
-# Traceback (most recent call last):
-#   File "/Users/EileenGonzales/anaconda/lib/python2.7/site-packages/IPython/core/interactiveshell.py", line 3066, in run_code
-#     exec(code_obj, self.user_global_ns, self.user_ns)
-#   File "<ipython-input-55-e03a4670e4f1>", line 1, in <module>
-#     mg=mc.make_model_db('btsettl', 'model_atmosphere_db', model_grid=models, grid_data='spec', param_lims=[('teff',400,1600,50),('logg',3.5,5.5,0.5)], fill_holes=False, bands=[], rebin_models=w,use_pandas=False)
-#   File "/Users/EileenGonzales/Dropbox/BDNYC/BDNYC_Research/Python/Modules/synth_fit/mcmc_fit.py", line 116, in make_model_db
-#     models = pd.DataFrame(model_grid)
-#   File "/Users/EileenGonzales/anaconda/lib/python2.7/site-packages/pandas/core/frame.py", line 226, in __init__
-#     mgr = self._init_dict(data, index, columns, dtype=dtype)
-#   File "/Users/EileenGonzales/anaconda/lib/python2.7/site-packages/pandas/core/frame.py", line 363, in _init_dict
-#     dtype=dtype)
-#   File "/Users/EileenGonzales/anaconda/lib/python2.7/site-packages/pandas/core/frame.py", line 5158, in _arrays_to_mgr
-#     index = extract_index(arrays)
-#   File "/Users/EileenGonzales/anaconda/lib/python2.7/site-packages/pandas/core/frame.py", line 5206, in extract_index
-#     raise ValueError('arrays must all be same length')
-# ValueError: arrays must all be same length
-
-
-
 # This does the actual fitting to the spectra
-bdsamp = mc.fit_spectrum([w,f,e], mg, 'btsettl', 'GJ758B', 25, 50, mask=[], db='', extents=None,object_name='Test',
+bdsamp = mc.fit_spectrum([w, f, e], mg, 'btsettl', 'GJ758B', 25, 50, mask=[], db='', extents=None, object_name='Test',
                          log=False, plot=True, prnt=True, generate=True, outfile=None)
